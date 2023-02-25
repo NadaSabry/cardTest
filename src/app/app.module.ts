05f6719c0888af4a2a37dae1bdce93d4673bfa34
Nada Sabry <Nada_201900894@fci.helwan.edu.eg>
2023-02-25 20:32:07 +0200

    test clone

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductCardComponent } from './Components/product-card/product-card.component';
+import { TestComponent } from './Components/test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductCardComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
